11df7e857b1e60f0ec648a6214ba56aa30347a1e
RetishM <88141231+RetishM@users.noreply.github.com>
2022-04-11 08:41:47 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,44 @@
-song="";
-song1="";
-function preload() {
-    song =loadSound("Arabic Kuthu.mp3");
-    song1 =loadSound("Samajavara gamana.mp3");
+Samajavara_gamana_song="";
+Arabic_Kuthu_song="";
+rightWrist_x = 0;
+rightWrist_y = 0;
+leftWrist_x = 0;
+leftWrist_y = 0;
 
-}
-function setup() {
-    canvas = createCanvas(600,500);
+function setup(){
+    canvas = createCanvas(600,530);
     canvas.center();
+
     video = createCapture(VIDEO);
-    video.hide;
+    video.hide();
+
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotposes);
 }
-function draw() {
-    image(video,0,0,600,500);
+
+function preload(){
+    Samajavara_gamana_song = loadSound("Arabic Kuthu.mp3");
+    Arabic_Kuthu_song = loadSound("Samajavara gamana.mp3");
+}
+
+function draw(){
+    image(video,0,0,600,530);
+}
+
+function modelLoaded(){
+    console.log("poseNet Is Initialized");
+}
+
+function gotposes(results){
+    if(results.length > 0){
+        console.log(results);
+         
+        leftWrist_x = results[0].pose.leftWrist.x;
+        leftWrist_y = results[0].pose.leftWrist.y;
+        console.log("leftWrist_x = "+leftWrist_x+" leftWrist_y = "+leftWrist_y);
+
+        rightWrist_x = results[0].pose.rightWrist.x;
+        rightWrist_y = results[0].pose.rightWrist.y;
+        console.log("rightWrist_x = "+rightWrist_x+" rightWrist_y = "+rightWrist_y);
+    }
 }
